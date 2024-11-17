38d733e16f651d85210e836ece713a4569a43437
YuRoGameDev <yurogamedev@gmail.com>
2024-11-17 17:11:53 -0500

    Update server.js

--- server.js
+++ server.js
@@ -136,14 +136,15 @@ app.use((req, res, next) => {
 //DELETE request that deletes an ID
 app.delete('/delete-game/:id', async function (req, res) {
   
-  const { id } = req.params;
+  const { user_id, game_name } = req.params;
   const client = new Client(clientConfig);
 
   try {
     await client.connect();
 
     const query = await client.query(
-      'DELETE FROM steam_user_activity WHERE id = $1 RETURNING *', [id]
+      'DELETE FROM steam_user_activity WHERE "user-id" = $1 AND "game-title" = $2 RETURNING *',
+      [userId, gameName]
     );
 
 
